5a44078f52343126380255d912ba622719b27627
Dao-you <qaz00639@gmail.com>
2023-12-28 01:31:25 +0800

    修正 symbol 的長度

--- userdata.h
+++ userdata.h
@@ -6,7 +6,7 @@
 struct _Userdata{
     char account[20]; //帳號
     char password[20]; //密碼
-    char symbol[1]; //管理員:m 老師:t 學生:s
+    char symbol[2]; //管理員:m 老師:t 學生:s
 };
 
 bool read_userdata_by_id(struct _Userdata *userdata,int id);
